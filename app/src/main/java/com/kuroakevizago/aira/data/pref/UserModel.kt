5ca28f1e5b71beacb0483b18a4f26b9c79ee2ded
Zaebi Agustia Hidayatullah <zaebiyt@gmail.com>
2024-12-03 21:10:10 +0700

    [Minor] User preference update

--- app/src/main/java/com/kuroakevizago/aira/data/pref/UserModel.kt
+++ app/src/main/java/com/kuroakevizago/aira/data/pref/UserModel.kt
@@ -1,6 +1,7 @@
 package com.kuroakevizago.dicodingstoryapp.data.pref
 
 data class UserModel(
+    val userId: String,
     val email: String,
     val token: String,
     val isLogin: Boolean = false
