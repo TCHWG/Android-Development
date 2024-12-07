febdfb2217cd7b0f7396c52b24f38eb055bfa7b5
Zaebi Agustia Hidayatullah <zaebiyt@gmail.com>
2024-12-08 00:28:01 +0700

    [Major] Detail Activity Update
    Fix shown data in the details activity

--- app/src/main/java/com/kuroakevizago/aira/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/kuroakevizago/aira/ui/detail/DetailActivity.kt
@@ -1,19 +1,25 @@
 package com.kuroakevizago.aira.ui.detail
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
-import android.view.View
-import android.view.animation.AnimationUtils
+import android.widget.ImageButton
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
 import com.kuroakevizago.aira.R
-import com.kuroakevizago.aira.adapter.MusicVerticalViewAdapter
+import com.kuroakevizago.aira.adapter.MusicVerticalViewAdapter.Companion.MUSIC_TAG
+import com.kuroakevizago.aira.data.local.entity.MusicEntity
 import com.kuroakevizago.aira.data.remote.response.MusicItem
 import com.kuroakevizago.aira.databinding.ActivityDetailBinding
 import com.kuroakevizago.aira.ui.ViewModelFactory
-import com.kuroakevizago.aira.ui.login.LoginActivity
+import com.kuroakevizago.aira.ui.auth.login.LoginActivity
 import com.kuroakevizago.aira.data.status.ResultStatus
+import com.kuroakevizago.aira.ui.music_player.MusicPlayerActivity
 
 class DetailActivity : AppCompatActivity() {
 
@@ -22,93 +28,194 @@ class DetailActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityDetailBinding
+    private var currentData: MusicItem? = null
+    private var userId: String? = null
+    private var isBookmarked = false
 
-    private var detailId: String = "-1"
-
+    @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Retrieve the music item from intent
+        val musicItem = intent.getParcelableExtra<MusicItem>(MUSIC_TAG)
+
+        if (musicItem != null) {
+            viewModel.initExistingData(musicItem)
+            bindMusicData(musicItem)
+            currentData = musicItem
+        }
 
         viewModel.getSession().observe(this) { user ->
             if (!user.isLogin) {
                 startActivity(Intent(this, LoginActivity::class.java))
                 finish()
             }
-        }
+            userId = user.userId
 
-        setupViewModelObserve()
-    }
 
-    @Suppress("DEPRECATION")
-    private fun setupViewModelObserve() {
-        val intent = this.intent
-        val musicItem = intent.getParcelableExtra<MusicItem>(MusicVerticalViewAdapter.MUSIC_TAG)
-//
-//        if (detailId != "" && !viewModel.isIdValid(detailId) && !viewModel.isFetched) {
-//            viewModel.fetchStory(detailId)
-//        }
-//
-//        viewModel.story.observe(this) { result ->
-//            when(result) {
-//                is ResultStatus.Error -> {
-//                    showErrorRetry(true)
-//                    setShimmerAnimation(false)
-//                }
-//                is ResultStatus.Loading -> {
-//                    showErrorRetry(false)
-//                    setShimmerAnimation(true)
-//                }
-//                is ResultStatus.Success -> {
-//                    showErrorRetry(false)
-//                    setShimmerAnimation(false)
-//                    val data = result.data.story
-//                    if (data != null) {
-//                        setDetail(data.name, data.description, data.photoUrl)
-//                    } else {
-//                        showErrorRetry(true)
-//                    }
-//                }
-//            }
-//
-//        }
-
-    }
-
-    private fun setDetail(title: String?, description: String?, imageUrl: String?) {
-        binding.textTitle.text = title
-        binding.textDescription.text = description
-        Glide.with(binding.root).load(imageUrl).into(binding.imageDetail)
-    }
-
-    private fun setShimmerAnimation(play: Boolean) {
-        val shimmerDetailLayout = binding.shimmerDetailLayout
-        val shimmerDetail = binding.shimmerDetail
-        val loadAnimation = AnimationUtils.loadAnimation(this, R.anim.shimmer_animation)
-
-        shimmerDetail.apply {
-            if (play) {
-                shimmerDetailLayout.visibility = View.VISIBLE
-                shimmerTvEventTitle.startAnimation(loadAnimation)
-                shimmerTvEventDescription.startAnimation(loadAnimation)
-                shimmerImgEventIcon.startAnimation(loadAnimation)
-            } else {
-                binding.shimmerDetailLayout.visibility = View.INVISIBLE
-                shimmerTvEventTitle.clearAnimation()
-                shimmerTvEventDescription.clearAnimation()
-                shimmerImgEventIcon.clearAnimation()
+            viewModel.isBookmarked(currentData?.id.toString(), userId!!).observe(this) {
+                isBookmarked = it
+                if (isBookmarked) {
+                    binding.bookmarkButton.setImageResource(R.drawable.ic_bookmark_filled_24px)
+                } else {
+                    binding.bookmarkButton.setImageResource(R.drawable.ic_bookmark_24px)
+                }
             }
         }
+
+        // Enable shared element transition
+        supportPostponeEnterTransition()
+        setupAction()
+        setupViewModelObserve()
+
+        // Start the transition after binding data
+        binding.root.post { supportStartPostponedEnterTransition() }
+
+    }
+
+    private fun setupAction() {
+        binding.playButton.setOnClickListener {
+            if (isInternetAvailable()) {
+                // Internet is available, proceed with the intent
+                val intent = Intent(this, MusicPlayerActivity::class.java)
+                intent.putExtra(MUSIC_TAG, currentData)
+                startActivity(intent)
+            } else {
+                // Show alert dialog if internet is not available
+                showNoInternetDialog()
+            }
+        }
+
+        binding.returnButton.setOnClickListener {
+            finish()
+        }
+
+        binding.refreshButton.setOnClickListener {
+            viewModel.fetchDetailData(currentData?.id.toString())
+        }
+
+        binding.bookmarkButton.setOnClickListener {
+            toggleBookmark(binding.bookmarkButton)
+        }
+    }
+
+    // Function to check internet availability
+    private fun isInternetAvailable(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork ?: return false
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+        return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+
+    // Function to show an alert dialog
+    private fun showNoInternetDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("No Internet Connection")
+            .setMessage("Internet connection is mandatory to play music. Please connect to the internet and try again.")
+            .setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss() // Dismiss the dialog when the user clicks OK
+            }
+            .setCancelable(false)
+            .show()
+    }
+
+    private fun bindMusicData(musicItem: MusicItem) {
+        // Bind data to the views
+        binding.titleText.text = musicItem.name
+        binding.summaryText.text = musicItem.musicDescription
+        binding.authorText.text = musicItem.author
+        binding.difficultyText.text = musicItem.difficulty
+    }
+
+    private fun toggleBookmark(button: ImageButton) {
+        // Animate the button with a scaling effect
+        val scaleUpX = ObjectAnimator.ofFloat(button, "scaleX", 1f, 1.2f)
+        val scaleUpY = ObjectAnimator.ofFloat(button, "scaleY", 1f, 1.2f)
+        val scaleDownX = ObjectAnimator.ofFloat(button, "scaleX", 1.2f, 1f)
+        val scaleDownY = ObjectAnimator.ofFloat(button, "scaleY", 1.2f, 1f)
+
+        val animatorSet = AnimatorSet()
+        animatorSet.play(scaleUpX).with(scaleUpY)
+        animatorSet.play(scaleDownX).after(scaleUpX)
+        animatorSet.play(scaleDownY).after(scaleUpY)
+
+        // Toggle bookmark state
+        isBookmarked = !isBookmarked
+        if (isBookmarked) {
+            button.setImageResource(R.drawable.ic_bookmark_filled_24px)
+
+            userId?.let {
+                currentData?.apply {
+                    viewModel.addBookmark(MusicEntity(
+                        userId = it,
+                        difficulty = difficulty,
+                        author = author,
+                        name = name ?: "",
+                        musicDescription = musicDescription,
+                        id = id.toString())
+                    )
+                }
+            }
+        } else {
+            button.setImageResource(R.drawable.ic_bookmark_24px)
+            viewModel.removeBookmark(currentData?.id.toString())
+        }
+
+        // Start the animation
+        animatorSet.duration = 300
+        animatorSet.start()
+    }
+
+    private fun setupViewModelObserve() {
+        viewModel.musicDetail.observe(this) { result ->
+            when(result) {
+                is ResultStatus.Error -> {
+//                    showErrorRetry(true)
+//                    setShimmerAnimation(false)
+                }
+                is ResultStatus.Loading -> {
+//                    showErrorRetry(false)
+//                    setShimmerAnimation(true)
+                }
+                is ResultStatus.Success -> {
+//                    showErrorRetry(false)
+//                    setShimmerAnimation(false)
+                    val data = result.data
+                    if (data.data != null) {
+                        bindMusicData(data.data)
+                        currentData = data.data
+                    }
+                }
+
+                else -> {}
+            }
+
+        }
+
+
+
     }
 //
-//    private fun showErrorRetry(show: Boolean) {
-//        binding.detailLayout.visibility = if (show) View.INVISIBLE else View.VISIBLE
-//        binding.errorDetailLayout.visibility = if (show) View.VISIBLE else View.GONE
-//        if (show) {
-//            binding.errorDetailRetry.btnRetryConnection.setOnClickListener {
-//                showErrorRetry(false)
-//                viewModel.fetchStory(detailId)
+//    private fun setShimmerAnimation(play: Boolean) {
+//        val shimmerDetailLayout = binding.shimmerDetailLayout
+//        val shimmerDetail = binding.shimmerDetail
+//        val loadAnimation = AnimationUtils.loadAnimation(this, R.anim.shimmer_animation)
+//
+//        shimmerDetail.apply {
+//            if (play) {
+//                shimmerDetailLayout.visibility = View.VISIBLE
+//                shimmerTvEventTitle.startAnimation(loadAnimation)
+//                shimmerTvEventDescription.startAnimation(loadAnimation)
+//                shimmerImgEventIcon.startAnimation(loadAnimation)
+//            } else {
+//                binding.shimmerDetailLayout.visibility = View.INVISIBLE
+//                shimmerTvEventTitle.clearAnimation()
+//                shimmerTvEventDescription.clearAnimation()
+//                shimmerImgEventIcon.clearAnimation()
 //            }
 //        }
 //    }
+//
 }
--- app/src/main/java/com/kuroakevizago/aira/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/kuroakevizago/aira/ui/detail/DetailViewModel.kt
@@ -1,28 +1,63 @@
 package com.kuroakevizago.aira.ui.detail
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.kuroakevizago.aira.data.UserRepository
+import com.kuroakevizago.aira.data.local.entity.MusicEntity
+import com.kuroakevizago.aira.data.remote.response.MusicItem
+import com.kuroakevizago.aira.data.remote.response.MusicResponse
+import com.kuroakevizago.aira.data.remote.response.MusicsResponse
+import com.kuroakevizago.aira.data.status.ResultStatus
 import com.kuroakevizago.dicodingstoryapp.data.pref.UserModel
+import kotlinx.coroutines.launch
 
 class DetailViewModel(private val repository: UserRepository) : ViewModel() {
 
-    private var currentId: String = ""
-    var isFetched = false
+    private val _musicDetail = MutableLiveData<ResultStatus<MusicResponse>>()
+    val musicDetail: LiveData<ResultStatus<MusicResponse>> = _musicDetail
+
+    var userId: String? = null
 
     fun getSession(): LiveData<UserModel> {
         return repository.getSession().asLiveData()
     }
 
-    fun isIdValid(newId: String): Boolean {
-        if (currentId != newId)
-        {
-            isFetched = false
-            return false
-        }
+    fun initExistingData(musicItem: MusicItem) {
+        _musicDetail.postValue(
+            ResultStatus.Success(MusicResponse(data = musicItem)
+            )
+        )
+    }
 
-        return true
+    fun fetchDetailData(id: String) {
+        viewModelScope.launch {
+            _musicDetail.postValue(ResultStatus.Loading)
+            val result = repository.getMusicDetail(id)
+            _musicDetail.postValue(result)
+        }
+    }
+
+    fun addBookmark(music: MusicEntity) {
+        viewModelScope.launch {
+            repository.insertBookmarkedMusic(music)
+        }
+    }
+
+    fun removeBookmark(musicId: String) {
+        viewModelScope.launch {
+            repository.removeBookmarkedMusic(musicId)
+        }
+    }
+
+    fun isBookmarked(musicId: String, userId: String): LiveData<Boolean> {
+        val result = MutableLiveData<Boolean>()
+        viewModelScope.launch {
+            result.value = repository.isBookmarked(musicId, userId)
+        }
+        return result
     }
 
 }
