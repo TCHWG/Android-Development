1a4667f38d3d0f653d6b2357f231ffe10adf8220
Zaebi Agustia Hidayatullah <zaebiyt@gmail.com>
2024-12-08 00:21:41 +0700

    [Minor] Utility Script updated

--- app/src/main/java/com/kuroakevizago/aira/utils/Utils.kt
+++ app/src/main/java/com/kuroakevizago/aira/utils/Utils.kt
@@ -3,9 +3,28 @@ package com.kuroakevizago.aira.utils
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.media.MediaMetadataRetriever
 import android.net.Uri
 import java.io.File
 import java.io.IOException
+
+// Function to retrieve the audio duration
+fun getAudioDuration(filePath: String): String {
+    val retriever = MediaMetadataRetriever()
+    return try {
+        retriever.setDataSource(filePath)
+        val durationMs = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)?.toLongOrNull() ?: 0L
+        retriever.release()
+
+        // Convert milliseconds to a formatted time string
+        val minutes = durationMs / 1000 / 60
+        val seconds = (durationMs / 1000 % 60).toInt()
+        String.format("%02d:%02d", minutes, seconds)
+    } catch (e: Exception) {
+        retriever.release()
+        "00:00"
+    }
+}
 
 fun getFileFromUri(context: Context, uri: Uri): File? {
     val contentResolver = context.contentResolver
